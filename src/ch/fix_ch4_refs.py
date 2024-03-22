5008631035e880e26cbe65ad51cf7e72b0610d6c
Bryan Fichera <bfichera@mit.edu>
2024-03-22 11:14:42 -0500

    Find citations to replace

---
+++ src/ch/fix_ch4_refs.py
@@ -0,0 +1,22 @@
+import re
+
+replace_dict = {
+    'Torchinsky2014ASymmetries': 'torchinsky_low_2014',
+    'Fichera2020SecondSymmetry': 'fichera_second_2020',
+    'Li2022High-pressureTaAs': 'li_high-pressure_2022',
+    'Shan2021GiantEngineering': 'shan_gian_2021',
+    'Boyd1992NonlinearOptics': 'boyd',
+    'Birss1966SymmetryMagnetism': 'birss',
+    'Petersen2006Nonlinear7': 'petersen_nonlinear_2006',
+    'Fichera2023Light-inducedSemiconductor': 'camn2bi2',
+    'Argibay2010AsymmetricEnvironment': 'argibay_asymmetric_2010',
+    'Ducuing1963ObservationCrystals': 'ducuing_observation_1963',
+}
+
+with open('ch4.tex', 'r') as fh:
+    lines = fh.readlines()
+
+for i, line in enumerate(lines):
+    match = re.findall('cite.{.*?}', line)
+    if match:
+        for m in match:
