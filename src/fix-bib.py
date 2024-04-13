364a2c339ee9d40fc61f7841f16e2eba4856a57e
Bryan Fichera <bfichera@mit.edu>
2024-04-13 18:11:53 -0500

    Add some corrections

--- src/fix-bib.py
+++ src/fix-bib.py
@@ -17,10 +17,28 @@ fixes = [
         '{Pseudogapped {Fermi} surfaces of 1{T}-\ce{TaS2} and 1{T}-\ce{TaSe2}: {A} charge density wave effect}',
     ],
     [
+        tastwolib,
+        'heinz_study_1985',
+        'title',
+        '{Study of \ce{Si}(111) Surfaces by Optical Second-Harmonic Generation: Reconstruction and Surface Phase Transformation}',
+    ],
+    [
+        tastwolib,
+        'teo2008',
+        'title',
+        '{Surface states and topological invariants in three-dimensional topological insulators: Application to \ce{Bi_{1-x}Sb_x}}',
+    ],
+    [
         cubr2lib,
         'essler_quasi-one-dimensional_1997',
         'title',
         '{Quasi-one-dimensional spin-1/2 {H}eisenberg magnets in their ordered phase: {C}orrelation functions}',
+    ],
+    [
+        cubr2lib,
+        'wang_nmr_2018',
+        'title',
+        '{{NMR} evidence of charge fluctuations in multiferroic \ce{CuBr2}',
     ],
     [
         cubr2lib,
@@ -47,10 +65,28 @@ fixes = [
         '{Probing the coupling between a coublon excitation and the charge-density wave in \ce{TaS2} by ultrafast optical spectroscopy}',
     ],
     [
+        tastwolib,
+        'wu_hexagonal_1989',
+        'title',
+        '{Hexagonal {Domain}-{Like} {Charge} {Density} {Wave} {Phase} of \ce{TaS2} {Determined} by {Scanning} {Tunneling} {Microscopy}}',
+    ],
+    [
         cubr2lib,
         'measson_amplitude_2014',
         'title',
         '{Amplitude {H}iggs mode in the 2{H}-\ce{NbSe2} superconductor}',
+    ],
+    [
+        cubr2lib,
+        'zhang_giant_2020',
+        'title',
+        '{Giant pressure-enhancement of multiferroicity in \ce{CuBr2}}',
+    ],
+    [
+        cubr2lib,
+        'zhao_cubr2_2012',
+        'title',
+        '{\ce{CuBr2} - {A} {New} {Multiferroic} {Material} with {High} {Critical} {Temperature}}',
     ],
     [
         thesislib,
@@ -70,4 +106,38 @@ fixes = [
         'title',
         '{Valley-selective optical {S}tark effect in monolayer \ce{WS2}}',
     ],
+    [
+        thesislib,
+        'wang_axial_2022',
+        'title',
+        '{Axial {Higgs} mode detected by quantum pathway interference in \ce{RTe3}}',
+    ],
+    [
+        thesislib,
+        'wilson_renormalization_1974',
+        'title',
+        'The renormalization group and the $\epsilon$ expansion',
+    ],
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
